6bffff47a7c157519fea416b9501ee0deee2f60b
unknown <Igor@.(none)>
2010-08-27 14:45:58 -0700

    Initial checkin

---
+++ Contract/IService1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace Contract {
+    interface IService1 {
+        // TODO: Implement the collection resource that will contain the SampleItem instances
+
+        [WebGet(UriTemplate = "")]
+        List<SampleItem> GetCollection();
+
+        [WebInvoke(UriTemplate = "", Method = "POST")]
+        SampleItem Create(SampleItem instance);
+
+        [WebGet(UriTemplate = "{id}")]
+        SampleItem Get(string id);
+
+        [WebInvoke(UriTemplate = "{id}", Method = "PUT")]
+        SampleItem Update(string id, SampleItem instance);
+
+        [WebInvoke(UriTemplate = "{id}", Method = "DELETE")]
+        void Delete(string id);
+    }
+}
---
+++ Contract/SampleItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Contract {
+    // TODO: Edit the SampleItem class
+    public class SampleItem {
+        public int Id { get; set; }
+        public string StringValue { get; set; }
+    }
+}
