1927ec803bcc50a5eb0a739c00990af660e066e0
unknown <Igor@.(none)>
2010-08-29 21:46:48 -0700

    Fix update/delete bugs

--- Client.WebApp/Default.aspx.designer.cs
+++ Client.WebApp/Default.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace Client.WebApp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// ObjectDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSource1;
     }
 }
