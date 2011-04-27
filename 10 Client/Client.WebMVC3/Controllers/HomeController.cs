073cb610fb5276bd39eb64e5d209f47ca7b0d745
Igor Okulist <Administrator@.(none)>
2011-04-27 13:58:48 -0700

    Display list of technobabble.

--- 10 Client/Client.WebMVC3/Controllers/HomeController.cs
+++ 10 Client/Client.WebMVC3/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ServiceModel;
 
 namespace Client.WebMVC3.Controllers {
     public class HomeController : Controller {
+
+        Web.Contract.ITechnobabble technobabble = null;
+
         public ActionResult Index() {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-            return View();
+            var factory = new ChannelFactory<Web.Contract.ITechnobabble>("restendpointname");
+            technobabble = factory.CreateChannel();
+
+            var list = technobabble.GetCollection();
+
+            return View(list);
         }
 
         public ActionResult About() {
