c1c933d024fb799d828d00483809d9d38387a65a
Igor Okulist <Administrator@.(none)>
2011-04-29 15:44:39 -0700

    Add edit handling.

--- 10 Client/Client.WebMVC3/Controllers/TechnobabbleController.cs
+++ 10 Client/Client.WebMVC3/Controllers/TechnobabbleController.cs
@@ -8,12 +8,10 @@ using System.ServiceModel;
 namespace Client.WebMVC3.Controllers {
     public class TechnobabbleController : Controller {
 
-        ChannelFactory<Web.Contract.ITechnobabble> factory =
+        static ChannelFactory<Web.Contract.ITechnobabble> factory =
             new ChannelFactory<Web.Contract.ITechnobabble>("restendpointname");
 
         static Web.Contract.ITechnobabble s_technobabble = null;
-
-        // GET: /Technobabble/
 
         public ActionResult Index() {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -23,11 +21,7 @@ namespace Client.WebMVC3.Controllers {
             var list = technobabble.GetCollection();
 
             return View(list);
-
         }
-
-        //
-        // GET: /Technobabble/Details/5
 
         public ActionResult Details(int id) {
             Web.Contract.ITechnobabble technobabble = NewMethod();
@@ -37,15 +31,9 @@ namespace Client.WebMVC3.Controllers {
             return View(item);
         }
 
-        //
-        // GET: /Technobabble/Create
-
         public ActionResult Create() {
             return View();
         }
-
-        //
-        // POST: /Technobabble/Create
 
         [HttpPost]
         public ActionResult Create(FormCollection collection) {
@@ -58,29 +46,25 @@ namespace Client.WebMVC3.Controllers {
             }
         }
 
-        //
-        // GET: /Technobabble/Edit/5
-
         public ActionResult Edit(int id) {
             return View();
         }
 
-        //
-        // POST: /Technobabble/Edit/5
-
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection) {
             try {
-                // TODO: Add update logic here
+                Web.Contract.ITechnobabble technobabble = NewMethod();
+
+                var item = technobabble.Get(id.ToString());
+                UpdateModel(item);
+
+                var updatedItem = technobabble.Update(id.ToString(), item);
 
                 return RedirectToAction("Index");
             } catch {
                 return View();
             }
         }
-
-        //
-        // GET: /Technobabble/Delete/5
 
         public ActionResult Delete(int id) {
             //Note: The "Delete" method here seems more of misnomer as this is more of user experience
